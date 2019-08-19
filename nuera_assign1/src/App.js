1646eed54965964718704252e332244de39059cb
Edbert Voo <developer@edbertvoo.com>
2019-08-19 15:09:47 -0600

    basic layout structure

--- nuera_assign1/src/App.js
+++ nuera_assign1/src/App.js
@@ -1,26 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import * as category from './entity/category'
-import { observer, inject }  from 'mobx-react';
-
+import * as category from "./entity/category";
+import { observer, inject } from "mobx-react";
+import Layout from "./component/layout/layout";
 
 class App extends React.Component {
-
-test = () => {
-
-this.props.itemStore.addItem("test", "test", category.Electronics);
-console.log(this.props.itemStore.itemList);
-}
-
+  test = () => {
+    this.props.itemStore.addItem("test", "test", category.Electronics);
+    console.log(this.props.itemStore.itemList);
+  };
 
   render() {
     return (
       <div className="App">
-<p>start</p>
+        <Layout />
       </div>
     );
   }
 }
 
-export default inject('itemStore')(observer(App));
+export default inject("itemStore")(observer(App));
