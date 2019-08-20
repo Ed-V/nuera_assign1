d1d9fb585a735e11b3a2cadf8459bf764333ed8f
Edbert Voo <developer@edbertvoo.com>
2019-08-20 13:24:02 -0600

    basic list completed

--- nuera_assign1/src/component/items/items.js
+++ nuera_assign1/src/component/items/items.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import Aux from "./../../hoc/Auxiliary";
 
-const items = (props) => (
-    <div>
-<button onClick={props.handleItemDelete.bind(this)}>Test me</button>
-    </div>
-);
+const items = props => {
+  const itemsDisplay = props.itemList.map(itemElement => (
+    <Aux>
+      <div className="columns">
+        <div className="column">{itemElement[0].category}</div>
+        <div className="column">
+          {props.categoryTotal(itemElement[0].category)}
+        </div>
+      </div>
+      
+      {itemElement.map(innerItemElement => (
+        <div className="columns">
+          <div className="column">{innerItemElement.name}</div>
+          <div className="column">${innerItemElement.value}</div>
+          <div className="column"><button onClick={props.handleItemDelete.bind(this)} data-itemRef={innerItemElement.id}>Remove</button></div>
+        </div>
+      ))}
+    </Aux>
+  ));
+
+  return <div>{itemsDisplay}</div>;
+};
 
 export default items;
